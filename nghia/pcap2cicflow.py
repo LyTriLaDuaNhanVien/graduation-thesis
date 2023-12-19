bbdd5f0c2791d5a4cc1dc119e4bb6e29889b83fa
Ubuntu <ubuntu@ip-172-31-28-228.ap-southeast-1.compute.internal>
2023-12-19 15:18:04 +0000

    update

--- nghia/pcap2cicflow.py
+++ nghia/pcap2cicflow.py
@@ -1,13 +1,14 @@
+import os
 import sys
 import argparse
 
 from cicflow_refactor import refactor
 
 def main(argv):
-    help_string = 'Usage: python3 ids.py --file <pcap file>'
+    help_string = 'Usage: python3 pcap2cicflow.py --file <pcap file>'
 
     parser = argparse.ArgumentParser(
-        description="DDoS Detection with CNN model"
+        description="Convert Pcap to CSV file"
     )
 
     parser.add_argument('-f', '--file', type=str, help='pcap file')
@@ -15,6 +16,11 @@ def main(argv):
     args = parser.parse_args(argv)
     print(args.file)
 
+    t = refactor.Convert(args.file)   
+    file = t.convert_to_dataframe()
+
+    filename = os.path.basename(args.file)
+    file.to_csv(filename.replace(".pcap", ".csv"),index=False)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
