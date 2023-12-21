b3f7c9335e832cf3c6a566806765b1ddf7e85fc1
Ubuntu <ubuntu@ip-172-31-28-228.ap-southeast-1.compute.internal>
2023-12-21 15:02:23 +0000

    Update Notebook

--- nghia/app/pages/analysis/__init__.py
+++ nghia/app/pages/analysis/__init__.py
@@ -1,8 +1,11 @@
 from pages import DataReader
 from .models import PredictAnalysis
 
-class Analysis(DataReader):
+class AnalysisCharts(DataReader):
     def get_prediction_chart(self):
-        model_path = 'path/to/model.pkl'  # Path to your ML model
-        analysis = PredictAnalysis(self.get_data(), model_path)
+        model_path = '../checkpoints/model.pkl'  # Path to your ML model
+        analysis = PredictAnalysis(
+            self.get_data(), 
+            model_path
+        )
         return analysis.create_prediction_chart()
--- nghia/app/pages/analysis/models.py
+++ nghia/app/pages/analysis/models.py
@@ -2,20 +2,11 @@
 import joblib
 import pandas as pd
 import matplotlib.pyplot as plt
-model = joblib.load('../../checkpoints/model.pkl')
-
-# def predict(data):
-#     reshaped_array = data.reshape(1, -1)
-#     result = model.predict(reshaped_array)
-#     if result[0] == -1:
-#         print("Malicious")
-#     else:
-#         print("Benign")
 
 class PredictAnalysis:
     def __init__(self, data, model_path):
         self.data = data
-        self.model = self.joblib.load(model_path)
+        self.model = joblib.load(model_path)
 
     def load_model(self, model_path):
         # Load the model from a .pkl file
@@ -26,7 +17,7 @@ class PredictAnalysis:
     def preprocess_data(self, data):
         # Drop columns to fit the model input shape
         # Update this list based on your model's requirements
-        columns_to_drop = ['src_ip', 'dst_ip','src_port']
+        columns_to_drop = ['src_ip', 'dst_ip','src_port','timestamp']
         return data.drop(columns=columns_to_drop, errors='ignore')
 
     def predict(self):
@@ -35,16 +26,22 @@ class PredictAnalysis:
 
         # Make predictions for each row
         predictions = self.model.predict(preprocessed_data)
+        print("predict:", predictions)
         return predictions
 
     def create_prediction_chart(self):
+
+        print("hello")
         # Generate predictions
         predictions = self.predict()
 
         # Plotting logic (modify as needed)
-        plt.figure()
-        plt.plot(predictions)
-        plt.title('Prediction Analysis')
-        plt.xlabel('X-axis')
-        plt.ylabel('Predictions')
-        return plt
+
+        for i in predictions:
+            print.write(i)
+        # plt.figure()
+        # plt.plot(predictions)
+        # plt.title('Prediction Analysis')
+        # plt.xlabel('X-axis')
+        # plt.ylabel('Predictions')
+        # return plt
