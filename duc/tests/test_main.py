e60f0823495ebe6f72beab5bad9a6b29b864b292
Ubuntu <ubuntu@ip-172-31-28-228.ap-southeast-1.compute.internal>
2023-12-28 16:00:08 +0000

    CI

--- duc/tests/test_main.py
+++ duc/tests/test_main.py
@@ -3,10 +3,12 @@ from service1.main import app  # Import your FastAPI app
 
 client = TestClient(app)
 
+
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
 
 def test_health_check():
     response = client.get("/health")
--- duc/web_bug/db.py
+++ duc/web_bug/db.py
@@ -1,16 +1,18 @@
 import sqlite3
 
 # Connect to SQLite database (or create it if it doesn't exist)
-conn = sqlite3.connect('db.sqlite3')
-print('1')
+conn = sqlite3.connect("db.sqlite3")
+print("1")
 # Create a cursor object
 c = conn.cursor()
-print('DB Init')
+print("DB Init")
 print(c)
 
 # Create table
-c.execute('''CREATE TABLE stocks
-             (date text, trans text, symbol text, qty real, price real)''')
+c.execute(
+    """CREATE TABLE stocks
+             (date text, trans text, symbol text, qty real, price real)"""
+)
 
 # Insert a row of data
 c.execute("INSERT INTO stocks VALUES ('2023-01-05','BUY','RHAT',100,35.14)")
